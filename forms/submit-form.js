03e0d4c2e021b976364bb55176f6d8ccf8b15e23
joellegg <joellegg@bellsouth.net>
2017-04-23 16:06:03 -0500

    testing regex to pull out API

--- forms/submit-form.js
+++ forms/submit-form.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 var http = require("http")
 const { appendFile } = require('fs')
 
@@ -16,44 +15,46 @@ var options = {
   }
 }
 
+let api_data = [];
+let chunk_merged = [];
+
 // Set up the request
 var req = http.request(options, function(res) {
-  console.log('Status: ' + res.statusCode)
-  console.log('Headers: ' + JSON.stringify(res.headers))
   res.setEncoding('utf8')
   res.on('data', function(chunk) {
-    appendFile('forms/response-apis.txt', `${chunk}`, (err) => {
+    // merge all the data chunks
+    chunk_merged.push(chunk)
+    appendFile('forms/chunks.txt', `${chunk}`, (err) => {
       if (err) throw err
     })
+    // use regex to pull the APIs from each chunk
+    const regex = /\d{2}-\d{3}-\d{5}/g
+    let API_chunk = chunk.match(regex);
+    // if there is an API in the string, push it to forms/response-apis.txt
+    if (API_chunk !== null) {
+      appendFile('forms/response-apis.txt', API_chunk, (err) => {
+        if (err) throw err
+      })
+      console.log('API_chunk', API_chunk);
+    }
+  })
+  // when the request is complete, log the entire response
+  res.on('end', function() {
+    console.log('chunk_merged', chunk_merged);
   })
 })
+
 req.on('error', function(e) {
   console.log('problem with request: ' + e.message)
 })
 
 // post the form data
-req.write(`factype=%27WELL%27&county=123&twp=&rng=&maxrec=100&Button1=Submit`)
+req.write(`factype=%27WELL%27&county=123&twp=&rng=&maxrec=1&Button1=Submit`)
 req.end()
 
 
-// factype:'WELL'
-// county:123
-// ApiCountyCode:
-// ApiSequenceCode:
-// Operator:
-// operator_name_number:name
-// Facility_Lease:
-// facility_name_number:name
-// qtrqtr:
-// sec:
-// twp:
-// rng:
-// Field:
-// field_name_number:name
-// maxrec:100
-// Button1:Submit
-
 // Take the response and pull out the APIs
+
 // Push the APIs into database
 
 
