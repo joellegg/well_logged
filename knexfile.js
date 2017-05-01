6c32ce3321e45d2e05a2c60df37d4a2a68d453be
joellegg <joellegg@bellsouth.net>
2017-05-01 18:08:19 -0500

    added dependencies, setting up files

---
+++ knexfile.js
@@ -0,0 +1,36 @@
+// update with your config
+
+module.exports = {
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/zoo_test',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/test'
+    }
+  },
+  development: {
+    client: 'pg',
+    debug: false,
+    connection: 'postgres://localhost/zoo',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/production'
+    }
+  }
+};
