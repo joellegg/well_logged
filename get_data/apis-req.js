1f75713ab682345b7b3662c420c4e1be39688c06
joellegg <joellegg@bellsouth.net>
2017-05-03 16:44:31 -0500

    set up code to retrieve data by county with 10K results max

--- get_data/apis-req.js
+++ get_data/apis-req.js
@@ -7,75 +7,80 @@ var path = require('path')
 
 // Weld Co. CO 1-12N 56-68W
 // TODO if you want more counties, change the i and j iteration for the T&Rs of interest
+let countyArray = ["001", "003", "005", "007", "009", "011", "013", "014", "015", "017", "019", "021", "023", "025", "027", "029", "031", "033", "035", "037", "041", "039", "043", "045", "047", "049", "051", "053", "055", "057", "059", "061", "063", "067", "065", "069", "071", "073", "075", "077", "079", "081", "083", "085", "087", "089", "091", "093", "095", "097", "099", "101", "103", "105", "107", "109", "111", "113", "115", "117", "119", "121", "123", "125"]
+
+for (let i = 0; i < countyArray.length; i++) {
+  console.log(i, 'county array', countyArray[i], typeof(countyArray[i]))
+  console.log('url', `factype=%27WELL%27&county=${countyArray[i]}&maxrec=10000&Button1=Submit`);
+}
 // and change req.write to remove or change the county
 // http://stackoverflow.com/questions/6158933/how-to-make-an-http-post-request-in-node-js
-for (let i = 10; i < 11; i++) {
-  for (let j = 60; j < 61; j++) {
-    // An object of options to indicate where to POST
-    var options = {
-      hostname: 'cogcc.state.co.us',
-      port: 80,
-      path: '/cogis/facilitysearch2.asp',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      }
+for (let i = 0; i < 1; i++) {
+  // An object of options to indicate where to POST
+  var options = {
+    hostname: 'cogcc.state.co.us',
+    port: 80,
+    path: '/cogis/facilitysearch2.asp',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
     }
+  }
 
-    let apiArray = [];
-    // read in the apis from temp_files/apis.json
-    readFile(path.join(__dirname, 'temp_files/apis.json'), 'utf8', (err, data) => {
-      if (err) throw err
-      apiArray = JSON.parse(data);
-      reqFunction();
-    })
-    let merge_chunk = '';
+  let apiArray = [];
+  // read in the apis from temp_files/apis.json
+  readFile(path.join(__dirname, 'temp_files/apis.json'), 'utf8', (err, data) => {
+    if (err) throw err
+    apiArray = JSON.parse(data);
+    reqFunction();
+  })
 
-    // Set up the request
-    function reqFunction() {
-      var req = http.request(options, (res) => {
-        res.setEncoding('utf8')
-        res.on('data', (chunk) => merge_chunk += chunk)
-          // when the request is complete, log the entire response
-        res.on('end', () => {
-          // pull out the apis using regex, chunk_APIs returns an array
-          const regex = /\d{2}-\d{3}-\d{5}/g
-          let chunk_APIs = merge_chunk.match(regex)
-            // TO DO if API exists, don't save it to the file
-            // loop through the chunk_APIs array to get each API
-          if (chunk_APIs !== null) {
-            // if the apiArray is empty, add just the new data
-            if (apiArray.length === 0) {
-              for (let k = 0; k < chunk_APIs.length; k++) {
-                let abv_api = chunk_APIs[k].replace(/(^.{2})*(-)/g, '')
+  // Set up the request
+  function reqFunction() {
+    var req = http.request(options, (res) => {
+      res.setEncoding('utf8')
+
+      let merge_chunk = '';
+      res.on('data', (chunk) => merge_chunk += chunk)
+        // when the request is complete, log the entire response
+      res.on('end', () => {
+        // pull out the apis using regex, chunk_APIs returns an array
+        const regex = /\d{2}-\d{3}-\d{5}/g
+        let chunk_APIs = merge_chunk.match(regex)
+          // TO DO if API exists, don't save it to the file
+          // loop through the chunk_APIs array to get each API
+        if (chunk_APIs !== null) {
+          // if the apiArray is empty, add just the new data
+          if (apiArray.length === 0) {
+            for (let k = 0; k < chunk_APIs.length; k++) {
+              let abv_api = chunk_APIs[k].replace(/(^.{2})*(-)/g, '')
+              apiArray.push({ api: `${chunk_APIs[k]}`, api_abv: `${abv_api}` })
+            }
+          }
+          // loop to exclude duplicates
+          else if (apiArray.length > 0) {
+            // loop through the chunk apis to see if they exist in the apiArray from apis.json
+            for (let k = 0; k < chunk_APIs.length; k++) {
+              let abv_api = chunk_APIs[k].replace(/(^.{2})*(-)/g, '')
+              let pos = apiArray.map(function(res) {
+                return res.api
+              }).indexOf(chunk_APIs[k])
+              if (pos === -1) {
                 apiArray.push({ api: `${chunk_APIs[k]}`, api_abv: `${abv_api}` })
               }
             }
-            // loop to exclude duplicates
-            else if (apiArray.length > 0) {
-              // loop through the chunk apis to see if they exist in the apiArray from apis.json
-              for (let k = 0; k < chunk_APIs.length; k++) {
-                let abv_api = chunk_APIs[k].replace(/(^.{2})*(-)/g, '')
-                let pos = apiArray.map(function(res) {
-                  return res.api
-                }).indexOf(chunk_APIs[k])
-                if (pos === -1) {
-                  apiArray.push({ api: `${chunk_APIs[k]}`, api_abv: `${abv_api}` })
-                }
-              }
-            }
-            // push array to file
-            writeFile('get_data/temp_files/apis.json', JSON.stringify(apiArray), (err) => {
-              if (err) throw err
-            })
           }
-        })
+          // push array to file
+          writeFile('get_data/temp_files/apis.json', JSON.stringify(apiArray), (err) => {
+            if (err) throw err
+          })
+        }
       })
-      req.on('error', (e) => console.log('problem with request: ' + e.message))
+    })
+    req.on('error', (e) => console.log('problem with request: ' + e.message))
 
-      // post the form
-      req.write(`factype=%27WELL%27&county=123&twp${i}N=&rng=${j}W&maxrec=100&Button1=Submit`)
-      req.end()
-    }
+    // post the form
+    req.write(`factype=%27WELL%27&county=${countyArray[i]}&maxrec=10000&Button1=Submit`)
+    req.end()
   }
 }
--- get_data/tiff-url-req.js
+++ get_data/tiff-url-req.js
@@ -6,15 +6,15 @@ var path = require('path')
 const { readFile, writeFile, appendFile } = require('fs')
 const cheerio = require('cheerio');
 
-// read in the APIs from local file
 let apis = []
 let api_data = []
+  // read in the APIs from local file
 readFile(path.join(__dirname, 'temp_files/apis.json'), 'utf8', (err, data) => {
-  if (err) throw err
-  apis = JSON.parse(data)
-  getApiData()
-})
-
+    if (err) throw err
+    apis = JSON.parse(data)
+    getApiData()
+  })
+  // read in the log_data
 function getApiData() {
   readFile(path.join(__dirname, '../db/seeds/log_data.json'), 'utf8', (err, data) => {
     if (err) throw err
@@ -23,31 +23,23 @@ function getApiData() {
   })
 }
 
-// TO DO loop through APIs to GET well logs and directional data if present
 function makeUrlReq() {
   for (let i = 0; i < apis.length; i++) {
-    // console.log('api', apis[i].api_abv)
-    // console.log('website', `http://cogcc.state.co.us/weblink/results.aspx?id=${apis[i].api_abv}`);
-    http.get(`http://cogcc.state.co.us/weblink/results.aspx?id=${apis[i].api_abv}`, (res) => {
-      const { statusCode } = res;
-      const contentType = res.headers['content-type']
+    console.log('api abv request', apis[i].api_abv)
 
-      let error;
-      if (statusCode !== 200) {
-        error = new Error(`Request Failed.\n` +
-          `Status Code: ${statusCode}`);
-      } else if (!/^text\/html/.test(contentType)) {
-        error = new Error(`Invalid content-type.\n` +
-          `Expected text/html but received ${contentType}`);
+    var options = {
+      hostname: 'cogcc.state.co.us',
+      port: 80,
+      path: '/weblink/',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
       }
-      if (error) {
-        console.error(error.message);
-        // consume response data to free up memory
-        res.resume();
-        return;
-      }
+    }
 
+    var req = http.request(options, (res) => {
       res.setEncoding('utf8');
+
       let rawData = '';
       // when data chunks are recieved, combine them
       res.on('data', (chunk) => { rawData += chunk; });
@@ -71,13 +63,12 @@ function makeUrlReq() {
                 doc_link: log_href
               }
 
-
               // see if the doc_link is already in the database
               let linkPos = api_data.map(function(res) {
                 return res.doc_link
               }).indexOf(log_href)
 
-              console.log('link position', linkPos)
+              console.log(apis[i].api, 'link position', linkPos)
 
               if (linkPos === -1) {
                 api_data.push(dataObj)
@@ -91,9 +82,13 @@ function makeUrlReq() {
           console.error(e.message)
         }
       });
-    }).on('error', (e) => {
-      console.error(`Got error: ${e.message}`);
-    });
+    })
+    req.on('error', (e) => console.log('problem with request: ' + e.message))
+
+    // post the form
+    req.write(`__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwUKLTQ1MjE3NDcxNQ9kFgICAw9kFgICAw9kFgICAQ9kFgJmD2QWEgIBDw8WAh4EVGV4dAUENTAwMGRkAgMPEA8WAh4LXyFEYXRhQm91bmRnZBAVDgASQnVzaW5lc3MgRG9jdW1lbnRzCUN1c3RvbWVycwtFbmZvcmNlbWVudApGYWNpbGl0aWVzBUZpZWxkCEhlYXJpbmdzCExvY2F0aW9uCE9wZXJhdG9yClByb2R1Y3Rpb24IUHJvamVjdHMHU2FtcGxlcwlXZWxsIExvZ3MFV2VsbHMVDgASQnVzaW5lc3MgRG9jdW1lbnRzCUN1c3RvbWVycwtFbmZvcmNlbWVudApGYWNpbGl0aWVzBUZpZWxkCEhlYXJpbmdzCExvY2F0aW9uCE9wZXJhdG9yClByb2R1Y3Rpb24IUHJvamVjdHMHU2FtcGxlcwlXZWxsIExvZ3MFV2VsbHMUKwMOZ2dnZ2dnZ2dnZ2dnZ2dkZAIJDxYCHg1XYXRlcm1hcmtUZXh0BQ1TZWxlY3QgYSBUeXBlZAIPDw8WAh4JTWF4TGVuZ3RoZmRkAhUPFhAeGUN1bHR1cmVEZWNpbWFsUGxhY2Vob2xkZXIFAS4eB0N1bHR1cmUFBWVuLVVTHhtDdWx0dXJlVGhvdXNhbmRzUGxhY2Vob2xkZXIFASweDkN1bHR1cmVEYXRlRk1UBQNNRFkeFkN1bHR1cmVBTVBNUGxhY2Vob2xkZXIFBUFNO1BNHhZDdWx0dXJlRGF0ZVBsYWNlaG9sZGVyBQEvHiBDdWx0dXJlQ3VycmVuY3lTeW1ib2xQbGFjZWhvbGRlcgUBJB4WQ3VsdHVyZVRpbWVQbGFjZWhvbGRlcgUBOmQCFw8PZBYuHg5Ub29sdGlwTWVzc2FnZQUMSW5wdXQgYSBkYXRlHhNNYXhpbXVtVmFsdWVNZXNzYWdlZR4TTWluaW11bVZhbHVlTWVzc2FnZWUeClZhbGlkRW1wdHkFBHRydWUeB0NlbnR1cnkFBDE5MDAeEmV2YWx1YXRpb25mdW5jdGlvbgUXTWFza2VkRWRpdFZhbGlkYXRvckRhdGUeGENsaWVudFZhbGlkYXRpb25GdW5jdGlvbmUeEExhc3RNYXNrUG9zaXRpb24FAjExHhFGaXJzdE1hc2tQb3NpdGlvbgUBMB4MTWluaW11bVZhbHVlZR4MTWF4aW11bVZhbHVlZR4KRGF0ZUZvcm1hdAUDTURZHhRWYWxpZGF0aW9uRXhwcmVzc2lvbmUeDElzTWFza2VkRWRpdAUEdHJ1ZR4IQ3NzRm9jdXMFD01hc2tlZEVkaXRGb2N1cx4TSW52YWxpZFZhbHVlTWVzc2FnZQUPRGF0ZSBpcyBpbnZhbGlkHg9UYXJnZXRWYWxpZGF0b3IFD3R4dERvY3VtZW50RGF0ZR4USW52YWxpZFZhbHVlQ3NzQ2xhc3MFD01hc2tlZEVkaXRFcnJvch4PQ3NzQmx1ck5lZ2F0aXZlBRZNYXNrZWRFZGl0Qmx1ck5lZ2F0aXZlHhBDc3NGb2N1c05lZ2F0aXZlBRdNYXNrZWRFZGl0Rm9jdXNOZWdhdGl2ZR4RRW1wdHlWYWx1ZU1lc3NhZ2UFEERhdGUgaXMgcmVxdWlyZWQeDEluaXRpYWxWYWx1ZWUeDURhdGVTZXBhcmF0b3IFAS9kAhkPDxYCHwNmZGQCHw8WEB8EBQEuHwUFBWVuLVVTHwYFASwfBwUDTURZHwgFBUFNO1BNHwkFAS8fCgUBJB8LBQE6ZAIhDw9kFi4fDAUMSW5wdXQgYSBkYXRlHw1lHw5lHw8FBHRydWUfEAUEMTkwMB8RBRdNYXNrZWRFZGl0VmFsaWRhdG9yRGF0ZR8SZR8TBQIxMR8UBQEwHxVlHxZlHxcFA01EWR8YZR8ZBQR0cnVlHxoFD01hc2tlZEVkaXRGb2N1cx8bBQ9EYXRlIGlzIGludmFsaWQfHAUPdHh0UmVsZWFzZWREYXRlHx0FD01hc2tlZEVkaXRFcnJvch8eBRZNYXNrZWRFZGl0Qmx1ck5lZ2F0aXZlHx8FF01hc2tlZEVkaXRGb2N1c05lZ2F0aXZlHyAFEERhdGUgaXMgcmVxdWlyZWQfIWUfIgUBL2QYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgQFB2J0bkluZm8FBmltZ0NhbAUMSW1hZ2VCdXR0b24xBQhidG5DbG9zZYVZfaAewkxjn8eDhig4Hs1dcCFes7vtcxy8UBmSov6d&__VIEWSTATEGENERATOR=C5CF1CF2&__EVENTVALIDATION=%2FwEdAB6m4w4fWlM6SvTi71dWCDjV3aJvDuFb%2F2p1hCWCRrS5YmbW8kck8nFk6519prMR6G7QrWrUDIWgy3Xo0kv3kL9jHht1ozL5BgleKvQPH6Cca0vS5Ds4FXn4d2mC%2B9EhvwuVBxaSygm6TPjGx4qUK%2BuWUlALSyaW9NpgE%2FWD8r8vQ2lVPmObft7bV3hj5qcdu8muov%2FhDnujsAFVaDS24HyzCoymaCNSbOK2jvLAdkuKgildBFffKeKTe1tb4%2B3%2B%2FJBs0x%2FAEZyx6sGp7HsV2rROwpmqj1uU0q1WcNRxkj6HedmY3m6t1%2FivZZafeFoWIC%2BgcBMNMEGd%2BSyKXRpsOkFjXfv98LOj71m6K1xzmyvEeKIrFpqoF9tBM4han22foiXsIg36X%2Fmd8dxlAnjFxv61iRvBsX8frBHbtKHAbgDhJKAjtwEjncTzWk9gkZ9SGF%2BJAaNKMjzBfagbeWiQhTLy3gQY%2BcjWU2bBUw1o6B8c24rGWAT5q3ZsqXx9n4EMLZLpkAKvHlFmeWUpJ6tT4i4qWMpmwNMT4y0NEL6HdPS3%2Brqic3S0ZDjCzqE9M2ZhdeQ18sL2VwHNiO0op9kbIMt9NpGzZOwh9RDF3%2FtMV5ai887FdbrBv%2FLhuLf7Yi2%2Bt89IXeEzBtf7U7%2Fj%2BVLcVpCBChLGVsPNmfGLO2sjMMa4hA%3D%3D&ddlClass=Well+Logs&txtUniqueIdentifier=${apis[i].api_abv}&waterUniqueIdentifier_ClientState=&txtDocumentNumber=&txtDocumentName=&txtDocumentDate=&MaskedEditExtender5_ClientState=&txtReleasedDate=&MaskedEditExtender1_ClientState=&SearchButton=Search&ptype=1`)
+
+    req.end()
   }
 }
 
