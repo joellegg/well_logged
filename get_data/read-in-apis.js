3072c2aa4af3e6828d05f987aee40746d24bc626
joellegg <joellegg@bellsouth.net>
2017-05-04 13:39:46 -0500

    looping through txt file of apis, add every 9k to new file that's dynamically created

--- get_data/read-in-apis.js
+++ get_data/read-in-apis.js
@@ -1,13 +1,19 @@
 var http = require("http")
-const { readFile, writeFile, appendFile } = require('fs')
+const { readFile, writeFile, appendFileSync } = require('fs')
 var path = require('path')
 
+// ************** READ IN FILES **************//
 let apis = ''
+let chunk_APIs = []
+
 readFile(path.join(__dirname, 'temp_files/TEST.txt'), 'utf8', (err, data) => {
   if (err) throw err
   apis = data;
+  const regex = /\d{2}-\d{3}-\d{5}/g
+  chunk_APIs = apis.match(regex)
   readNext();
 })
+
 
 let apiArray = []
 
@@ -19,17 +25,30 @@ function readNext() {
   })
 }
 
-function writeFunction() {
-  const regex = /\d{2}-\d{3}-\d{5}/g
-  let chunk_APIs = apis.match(regex)
+let j = 0;
 
+function writeFunction() {
   for (let k = 0; k < chunk_APIs.length; k++) {
+    console.log(k)
     let abv_api = chunk_APIs[k].replace(/(^.{2})*(-)/g, '')
-    let pos = apiArray.map(function(res) {
-      return res.api
-    }).indexOf(chunk_APIs[k])
-    if (pos === -1) {
-      apiArray.push({ api: `${chunk_APIs[k]}`, api_abv: `${abv_api}` })
+      // let pos = apiArray.map(function(res) {
+      //   return res.api
+      // }).indexOf(chunk_APIs[k])
+      // if (pos === -1) {
+    let dataObj = { api: `${chunk_APIs[k]}`, api_abv: `${abv_api}` }
+      //   apiArray.push(dataObj)
+      // }
+      // if (k % 5000 === 0 && k !== 0) {
+    appendFileSync(`get_data/temp_files/apis_${j}.json`, JSON.stringify(dataObj) + ",\r\n", (err) => {
+        if (err) throw err
+          // console.log('wrote data')
+          // j++
+          // apiArray = []
+      })
+      // }
+    if (k % 9000 === 0 && k !== 0) {
+      console.log(j, k, chunk_APIs[k])
+      j++
     }
   }
 }
