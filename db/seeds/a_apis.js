743a5c5a59838787396cc7e29c333520ee04e115
joellegg <joellegg@bellsouth.net>
2017-05-11 11:14:24 -0500

    working on seeding db with a bunch of promises

--- db/seeds/a_apis.js
+++ db/seeds/a_apis.js
@@ -4,58 +4,57 @@ const { knex } = require('../database');
 const { readFileSync, readdir } = require('fs')
 const path = require('path')
 
-let logFileCount = 71
-let apiData = []
-let apiPromise;
-
-function getLogCount() {
-
-  console.log('getting file count')
-  const dir = (path.join(__dirname, '../log-data'))
-  readdir(dir, (err, files) => {
-    logFileCount = files.length
-    console.log('# o files', logFileCount)
+function getFiles() {
+  return new Promise(function(res, rej) {
+    console.log('getting file count')
+    const dir = (path.join(__dirname, '../log-data'))
+    readdir(dir, (err, files) => {
+      let logFileCount = files.length
+      console.log('# o files', logFileCount)
+      res(logFileCount)
+    })
   })
-
-  readLogData()
 }
-getLogCount()
-
 
 // read in API data
-function readLogData() {
-  console.log('reading in data')
-  for (let i = 0; i < logFileCount; i++) {
-    try {
-      let data = readFileSync(path.join(__dirname, `../db/log-data/log_data_${i}.json`))
-      apiData.push.apply(apiData, JSON.parse(data))
-    } catch (err) {
-      if (err.code !== 'ENOENT') {
-        throw err;
+function readApis(logFileCount) {
+  return new Promise(function(res, rej) {
+    console.log('reading in data')
+    let apiData = []
+    for (let i = 0; i < logFileCount; i++) {
+      try {
+        let data = readFileSync(path.join(__dirname, `../db/log-data/log_data_${i}.json`))
+        apiData.push.apply(apiData, JSON.parse(data))
+      } catch (err) {
+        if (err.code !== 'ENOENT') {
+          throw err;
+        }
       }
     }
-  }
-  promiseItAll()
-}
-
-
-function promiseItAll() {
-  console.log('promising it all')
-  apiPromise = apiData.map(({ api, api_abv, doc_type, doc_link }) => {
-    return knex('api_docs').insert({ api, api_abv, doc_type, doc_link })
+    console.log('apiData length', apiData.length)
+    res(apiData)
   })
-  runDaSeeds()
 }
 
-function runDaSeeds() {
-  exports.seed = function(knex, Promise) {
-    console.log('seeding file')
-    return knex('api_docs')
-      .then(function() {
-        console.log('api promise', apiPromise)
-        return Promise.all(apiPromise)
+exports.seed = function(knex, Promise) {
+  getFiles()
+    .then((logFileCount) => {
+      return readApis(logFileCount)
+    })
+    .then((apiData) => {
+      console.log('promising it all')
+      let apiPromise = apiData.map(({ api, api_abv, doc_type, doc_link }) => {
+        return knex('api_docs').insert({ api, api_abv, doc_type, doc_link })
       })
-  }
+    })
+    .then((apiPromise) => {
+      console.log('seeding file')
+      return knex('api_docs')
+        .then(function() {
+          console.log('api promise', apiPromise)
+          return Promise.all(apiPromise)
+        })
+    })
 }
 
 // exports.seed = function(knex, Promise) {
